3f77b0ee6b4f95bbdaed1442f2721be382c5f4dc
Art Mills <art@hamcois.com>
2024-04-09 18:27:33 -0500

    auth_app logout working

--- hamco/hamco/settings.py
+++ hamco/hamco/settings.py
@@ -146,3 +146,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Override the default login redirect
+LOGIN_REDIRECT_URL = '/'
+# Override the default logout redirect
+LOGOUT_REDIRECT_URL = '/auth/login'
