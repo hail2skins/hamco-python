6e27e32a10d88e97545ba69a16a51cf9e8e86828
Art Mills <art@hamcois.com>
2024-04-09 16:08:08 -0500

    Basic website app now configured and app working, I believe.

---
+++ hamco/website/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+# Import the views from the website app
+from . import views
+
+urlpatterns = [
+        
+    # Path to the home view
+    path("", views.home, name="")
+]
