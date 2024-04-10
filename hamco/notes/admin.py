9702749387cb8ca94c4b4783daaba35d59a5c5f8
Art Mills <art@hamcois.com>
2024-04-10 10:12:18 -0500

    Note added to admin with customization. This will be used a lot.

--- hamco/notes/admin.py
+++ hamco/notes/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+# Import truncator
+from django.utils.text import Truncator
+
 # Register your models here.
+from . models import Note
+
+# Register the Note model with the admin site
+# This is the simplest way to register a model with the admin site
+#admin.site.register(Note)
+
+
+# Register the Note model with the admin site with customization
+# Customizing the admin interface for the note model
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'truncated_content', 'created_at', 'updated_at', 'deleted_at',)
+    list_filter = ('created_at', 'updated_at', 'deleted_at',)
+    search_fields = ('title', 'content',)
+    #prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    ordering = ('created_at',)
+    
+    # Custom method to truncate the content
+    def truncated_content(self, obj):
+        return Truncator(obj.content).words(15, truncate='...')
+    truncated_content.short_description = 'Content'
