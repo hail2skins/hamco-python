b5198b1f3066fa0ebf2bc6df0bfd51ea5de87eb3
Art Mills <art@hamcois.com>
2024-04-10 15:35:26 -0500

    [#187402714][#187407409] Last 5 on front and view note linked.  Ready.

--- hamco/website/views.py
+++ hamco/website/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+# Import the Note model to run queryset 
+# Note models are in the notes app, not the website app
+from notes.models import Note
+
+
 # Create your views here.
 def home(request):
+    
+    # Queryset for the last five notes ordered by created_at
+    latest_notes = Note.objects.order_by('-created_at')[:5]
 
     # Context dictionary
     context = {
@@ -9,6 +17,7 @@ def home(request):
         'company': 'Hamco Internet Solutions',
         'heading': 'Stuff I Talk About',
         'subheading': 'That will not get me fired.',
+        'latest_notes': latest_notes,
     }
 
     return render(request, 'website/index.html', context)
